79e2874e9e95d505d25960dcedf138e890451950
juanmcel <juanmcel2697@gmail.com>
2019-02-26 11:40:36 -0600

    Actualizacion vista settings

--- src/app/display/display.page.ts
+++ src/app/display/display.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterContentInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
 import {Chart} from 'chart.js'
+import {MonitorService} from './../services/monitor.service';
+import { Monitor } from './../interfaces/monitor';
+import { Arrays } from '../interfaces/arrays';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-display',
@@ -11,9 +16,9 @@ export class DisplayPage implements OnInit {
   myChart: Chart;
   @ViewChild('chartContainer') chartcontainer: ElementRef;
   @ViewChild('chartcanvas') chartcanvas: ElementRef;
-
+  monitors: Observable<Monitor[]>;
+  arrays: Observable<Arrays[]>;
   ngAfterViewInit() {
-
     this.createChart();
 
   }
@@ -21,10 +26,10 @@ export class DisplayPage implements OnInit {
     this.myChart = new Chart(this.chartcanvas.nativeElement, {
       type: 'line',
       data: {
-        labels: ['2019/02/01', '2019/02/08', '2019/02/15', '2019/02/22', '2019/02/29', '2019/03/05'],
+        labels: ["12-ene","12-ene","12-ene","12-ene","12-ene","12-ene"],
         datasets: [{
           label: 'Pesos en KG',
-          data: [32, 35, 38, 42, 46, 50],
+          data: [30,32,32,34,36,38],
           borderWidth: 1,
           backgroundColor: ['#E78839'],
           pointBackgroundColor: ['#6E4217','#6E4217','#6E4217','#6E4217','#6E4217','#6E4217']
@@ -42,11 +47,40 @@ export class DisplayPage implements OnInit {
     });
   }
 
-  constructor(public navCtrl: NavController) { 
+  constructor(public navCtrl: NavController,public ms: MonitorService, public alertCtrl: AlertController) {
+    this.ms.loadMonitor();
+    this.ms.loadArrays();
+    console.log(this.monitors);    
+   }
 
+  async doPrompt() {
+    const prompt = await this.alertCtrl.create({
+      header: "Comentar",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Observación'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Guardar',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    await prompt.present();
   }
 
   ngOnInit() {
+        //console.log(this.todos);
   }
-
 }
